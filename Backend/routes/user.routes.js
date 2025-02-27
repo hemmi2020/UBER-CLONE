aee238c8becca9c58838a3ea848421fe18c842ff
hemmi2020 <hamzashahzad020@gmail.com>
2025-02-28 02:12:06 +0500

    added files

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -12,5 +12,12 @@ router.post('/register', [
     userController.registerUser
 )
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be atleast 6 characters long')
+],
+    userController.loginUser
+)
+router.get('/profile', userController.getUserProfile)
 
 module.exports = router;
