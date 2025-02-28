cd889648454a84b41023f4260fe4f9627252e088
hemmi2020 <hamzashahzad020@gmail.com>
2025-02-28 23:14:27 +0500

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes')
+const cookieParser = require('cookie-parser')
 
 connectToDb();
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello world')
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller');
+const authMiddleWare = require('../middlewares/auth.middleware')
 
 
 router.post('/register', [
@@ -18,6 +19,7 @@ router.post('/login', [
 ],
     userController.loginUser
 )
-router.get('/profile', userController.getUserProfile)
+router.get('/profile',authMiddleWare.authUser, userController.getUserProfile)
+router.get('/logout',authMiddleWare.authUser, userController.logoutUser)
 
 module.exports = router;
